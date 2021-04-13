492e456db4dad7b4e77638fa1ca51bc360ff7462
Nam Jueun <45748632+namjueun@users.noreply.github.com>
2021-04-14 01:16:41 +0900

    2021.04.14

--- perceptron.py
+++ perceptron.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[2]:
 
 
 import tensorflow as tf
@@ -11,7 +11,7 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.lossess import mse
+from tensorflow.keras.losses import mse
 
 # 데이터 준비하기
 x = np.array([[0,0],[1,0],[0,1],[1,1]])
@@ -22,4 +22,16 @@ model = Sequential()
 
 # 단층 퍼셉트론을 구성합니다.
 model.add(Dense(1,input_shape = (2, ), activation = 'linear'))
+
+# 모델 준비하기 
+model.compile(optimizer = SGD(), loss = mse, metrics = ['acc']) # list 형태로 평가지표를 전달합니다.
+
+#학습시키기 
+model.fit(x,y, epochs = 500)
+
+
+# In[ ]:
+
+
+
 
